8a04cce3ef0a1f4c3abeb99203d0c695118ed2ad
Elona Atroshchenko <ell.atroshchenko@gmail.com>
2020-09-04 23:01:29 +0300

    Задачи модуль 2

---
+++ js/task-2.js
@@ -0,0 +1,24 @@
+// Напиши скрипт подсчета стоимости гравировки украшений.Для этого создай функцию calculateEngravingPrice(message, pricePerWord) принимающую строку(в строке будут только слова и пробелы) и цену гравировки одного слова, и возвращающую цену гравировки всех слов в строке.
+let total;
+const calculateEngravingPrice = function (message, pricePerWord) {
+    total = message.split(' ').length * pricePerWord;
+    return total;
+};
+
+
+console.log(calculateEngravingPrice('Proin sociis natoque et magnis parturient montes mus', 10, ), );
+
+console.log(
+    calculateEngravingPrice(
+        'Proin sociis natoque et magnis parturient montes mus',
+        20,
+    ),
+);
+
+console.log(
+    calculateEngravingPrice('Donec orci lectus aliquam est magnis', 40),
+);
+
+console.log(
+    calculateEngravingPrice('Donec orci lectus aliquam est magnis', 20),
+);
